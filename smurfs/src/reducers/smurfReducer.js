b9a8de42c912bc6c13a823f4a0a189a6b57cbed7
Jabin <jabinwashburn@gmail.com>
2020-08-14 15:50:48 -0700

    i have a few things up and running, have some actionsmade and info displaying in the console

--- smurfs/src/reducers/smurfReducer.js
+++
@@ -1,5 +0,0 @@
-const initialState = {
-    smurfs: [],
-    loading: false, 
-    error: ''
-}
---
+++ smurfs/src/store/reducers/smurfReducer.js
@@ -0,0 +1,31 @@
+import { SMURF_LOADING, SMURF_SUCCESS, SMURF_FAIL } from '../actions/smurfActions'
+
+const initialState = {
+    smurfs: [],
+    loading: false, 
+    error: ''
+}
+
+export const useReducer = (state = initialState, action) => {
+    switch(action.type){
+        case SMURF_LOADING:
+            return{
+                ...state,
+                loading: true, 
+            }
+            case SMURF_SUCCESS:
+                return{
+                    ...state,
+                    smurfs: action.payload,
+                    loading: false,
+                }
+            case SMURF_FAIL:
+                return{
+                    ...state,
+                    error: action.payload,
+                    loading: false
+                }
+        default:
+            return state
+    }
+}
